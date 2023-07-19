b7a6cde100b8da38bd40e858f41662599abf0874
Johanna Busch <56266543+johannabusch@users.noreply.github.com>
2023-07-19 14:49:21 +0200

    changed path in mk_bnp.sh

--- example/mk_bnp.sh
+++ example/mk_bnp.sh
@@ -1,4 +1,4 @@
+#!/bin/bash
 
-
-cat ct_luzar_chandler_1024W_298K.dat | ./correct_bnp.pl -D 0.00414 -b 3.14194 -h 0.00093 > ct_bnp_298K.dat 
-cat ct_luzar_chandler_1024W_273K.dat | ./correct_bnp.pl -D 0.00202 -b 3.12924 -h 0.00095 > ct_bnp_273K.dat 
+cat ct_luzar_chandler_1024W_298K.dat | ../src/correct_bnp.pl -D 0.00414 -b 3.14194 -h 0.00093 > ct_bnp_298K.dat 
+cat ct_luzar_chandler_1024W_273K.dat | ../src/correct_bnp.pl -D 0.00202 -b 3.12924 -h 0.00095 > ct_bnp_273K.dat 
